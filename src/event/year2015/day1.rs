30d3458e808976cdaf7a3f692eff5de04af9b1c2
yinx <tollemerkevin@gmail.com>
2025-01-14 17:59:36 +0100

    Year 2015 - Day 1 - Puzzle 2

--- src/event/year2015/day1.rs
+++ src/event/year2015/day1.rs
@@ -2,12 +2,12 @@ use std::{fs, io};
 
 pub fn day1() {
     println!("Day 1");
-    part1();
+    let input = read_file().expect("Unable to read file!");
+    part1(&input);
+    part2(&input);
 }
 
-pub fn part1() {
-    let input = read_file().expect("Unable to read file!");
-
+pub fn part1(input: &str) {
     let mut floor = 0;
     for c in input.chars() {
         match c {
@@ -19,6 +19,23 @@ pub fn part1() {
         }
     }
     println!("Part 1: {}", floor);
+}
+
+pub fn part2(input: &str) {
+    let mut floor = 0;
+    for (i, c) in input.chars().enumerate() {
+        match c { 
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {
+                eprintln!("Unknown character found");
+            }
+        }
+        if floor == -1 {
+            println!("Part 2: {}", i + 1);
+            break;
+        }
+    }
 }
 
 fn read_file() -> io::Result<String> {
