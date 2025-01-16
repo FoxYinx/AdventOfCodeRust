cee56c03dda310c34efd7f969769b67261b87c69
yinx <tollemerkevin@gmail.com>
2025-01-16 21:40:06 +0100

    Year 2015 - Day 4

--- src/event/year2015/day4.rs
+++ src/event/year2015/day4.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use md5::{Md5, Digest};
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 pub fn part1() -> i32 {
     answer(5)
@@ -11,16 +12,11 @@ pub fn part2() -> i32 {
 
 fn answer(nb: usize) -> i32 {
     let input = read_file().expect("Unable to read file!");
-    let mut i = 0;
-    loop {
+    (0..1_500_000).into_par_iter().find_first(|&i| {
         let hash = Md5::digest(input.clone() + i.to_string().as_str());
         let hex_hash = base16ct::lower::encode_string(&hash);
-        if hex_hash[..nb] == "0".repeat(nb) {
-            break;
-        }
-        i += 1;
-    }
-    i
+        hex_hash[..nb] == "0".repeat(nb)
+    }).expect("No valid hash found")
 }
 
 fn read_file() -> io::Result<String> {
