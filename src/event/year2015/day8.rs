2f7e1668323025ed6ee3fba408c4f024de794013
yinx <tollemerkevin@gmail.com>
2025-01-16 18:11:49 +0100

    Year 2015 - Day 8 - Puzzle 2

--- src/event/year2015/day8.rs
+++ src/event/year2015/day8.rs
@@ -15,6 +15,18 @@ pub fn part1() -> usize {
     total
 }
 
+pub fn part2() -> usize {
+    let input = read_file().expect("Unable to read file!");
+    let mut total: usize = 0;
+    for line in input {
+        total += 2;
+        total += line.matches('"').count();
+        total += line.matches('\\').count();
+    }
+    
+    total
+}
+
 fn read_file() -> io::Result<Vec<String>> {
     BufReader::new(File::open("ressources/year2015/day8.txt")?).lines().collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn main() {
     time(7, 1, event::year2015::day7::part1);
     time(7, 2, event::year2015::day7::part2);
     time(8, 1, event::year2015::day8::part1);
+    time(8, 2, event::year2015::day8::part2);
     
     let end = Instant::now();
     println!("Total Time: {:?}", end.duration_since(start));
