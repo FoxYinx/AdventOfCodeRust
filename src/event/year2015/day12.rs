2adc276664cd6adf6f36c7cec8f66bb9a5ac0fa3
yinx <tollemerkevin@gmail.com>
2025-01-18 15:33:52 +0100

    Year 2015 - Day 12 - Puzzle 2

--- src/event/year2015/day12.rs
+++ src/event/year2015/day12.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 use regex::Regex;
+use serde_json::Value;
 
 pub fn part1() -> i32 {
     let line = read_file().expect("Unable to read file!");
@@ -7,7 +8,28 @@ pub fn part1() -> i32 {
     let answer: i32 = regex.captures_iter(&line).map(|num| {
         num[1].parse::<i32>().unwrap()
     }).sum();
+
     answer
+}
+
+pub fn part2() -> i64 {
+    let line = read_file().expect("Unable to read file!");
+    let data: Value = serde_json::from_str(line.as_str()).expect("Unvalid json");
+    calculate_sum_without_red(&data)
+}
+
+fn calculate_sum_without_red(data: &Value) -> i64 {
+    match data {
+        Value::Number(num) => num.as_i64().unwrap_or(0),
+        Value::Array(array) => array.iter().map(calculate_sum_without_red).sum(),
+        Value::Object(obj) => {
+            if obj.values().any(|v| v == "red") {
+                return 0;
+            }
+            obj.values().map(calculate_sum_without_red).sum()
+        }
+        _ => 0
+    }
 }
 
 fn read_file() -> io::Result<String> {
--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ fn main() {
     time(11, 1, event::year2015::day11::part1);
     time(11, 2, event::year2015::day11::part2);
     time(12, 1, event::year2015::day12::part1);
+    time(12, 2, event::year2015::day12::part2);
     
     let end = Instant::now();
     println!("Total Time: {:?}", end.duration_since(start));
