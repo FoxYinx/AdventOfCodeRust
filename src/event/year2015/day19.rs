86c8a7462a70a4a6c189035d325cf5d414ce9f55
yinx <tollemerkevin@gmail.com>
2025-01-23 18:02:19 +0100

    Year 2015 - Day 19 - Puzzle 2

--- src/event/year2015/day19.rs
+++ src/event/year2015/day19.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::{fs, io};
@@ -33,14 +33,27 @@ pub fn part2() -> usize {
     let regex = Regex::new(r"(\w+) => (\w+)").unwrap();
     let mut replacements: Vec<(String, String)> = Vec::new();
     regex.captures_iter(&lines).for_each(|cap| {
-        let key = cap[1].to_string();
-        let value = cap[2].to_string();
-        replacements.push((key, value));
+        let lhs = cap[1].to_string();
+        let rhs = cap[2].to_string();
+        replacements.push((lhs, rhs));
     });
-    let goal = get_initial_molecule().expect("Unable to get last line!");
-    
-    0
+    let mut start = get_initial_molecule().expect("Unable to get last line!");
+    let mut count = 0;
+    loop {
+        for (key, value) in &replacements {
+            if let Some(pos) = start.find(value) {
+                let (left, right) = start.split_at(pos);
+                let right = right.to_string().split_off(value.len());
+                start = format!("{left}{key}{right}");
+                count += 1;
+            }
+        }
+        if start == "e" {
+            return count;
+        }
+    }
 }
+
 
 fn read_lines() -> io::Result<String> {
     fs::read_to_string("ressources/year2015/day19.txt")
