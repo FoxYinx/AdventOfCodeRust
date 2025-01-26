ef0098820f43cba77a2d93984e5243d49fae863c
yinx <tollemerkevin@gmail.com>
2025-01-26 20:14:02 +0100

    Year 2016 - Day 3 - Puzzle 1

--- src/event/year2016/day3.rs
+++ src/event/year2016/day3.rs
@@ -1,18 +1,16 @@
 use std::fs;
-use regex::Regex;
 
 pub fn part1() -> u16 {
     let mut valid = u16::MIN;
-    let lines = fs::read_to_string("ressources/year2016/day3.txt").expect("Unable to read file!");
-    let regex = Regex::new(r"(\d+) *(\d+) *(\d+)").expect("Unable to make regex");
-    regex.captures_iter(&lines).for_each(|cap| {
-        let s1 = cap[1].parse::<u16>().unwrap();
-        let s2 = cap[2].parse::<u16>().unwrap();
-        let s3 = cap[3].parse::<u16>().unwrap();
-        if s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1 {
+    let file = fs::read_to_string("ressources/year2016/day3.txt").expect("Unable to read file!");
+    for line in file.lines() {
+        let parts: Vec<u16> = line.split_whitespace().map(|s| {
+            s.parse().unwrap()
+        }).collect();
+        if parts[0] + parts[1] > parts[2] && parts[0] + parts[2] > parts[1] && parts[1] + parts[2] > parts[0] {
             valid += 1;
         }
-    });
+    }
     
     valid
 }
