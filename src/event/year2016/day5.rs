ff2ca64f2efaaad6cf00e01b9fb46beb3cb308f9
yinx <tollemerkevin@gmail.com>
2025-01-27 20:38:12 +0100

    Year 2016 - Day 5

--- src/event/year2016/day5.rs
+++ src/event/year2016/day5.rs
@@ -4,7 +4,7 @@ use rayon::prelude::*;
 
 pub fn part1() -> String {
     let door_id = fs::read_to_string("resources/year2016/day5.txt").expect("Unable to read file!");
-    let output = (0..10_500_000)
+    let output = (0..8_500_000)
         .into_par_iter()
         .map(|i| base16ct::lower::encode_string(&Md5::digest(format!("{door_id}{i}").as_str())))
         .filter(|hash| hash.starts_with("00000"))
@@ -25,22 +25,34 @@ pub fn part1() -> String {
 
 pub fn part2() -> String {
     let door_id = fs::read_to_string("resources/year2016/day5.txt").expect("Unable to read file!");
-    let mut output: [char; 8] = ['_'; 8];
-    for i in 0.. {
-        let hash = base16ct::lower::encode_string(&Md5::digest(format!("{door_id}{i}").as_str()));
-        if hash.starts_with("00000") {
-            if let Some(pos) = hash.chars().nth(5).unwrap().to_digit(10) {
-                if pos < 8 {
-                    let pos = pos as usize;
-                    if output[pos] == '_' {
-                        output[pos] = hash.chars().nth(6).unwrap();
-                        if !output.contains(&'_') {
-                            break;
-                        }
+    let output = (0..26_000_000)
+        .into_par_iter()
+        .map(|i| {
+            let hash = base16ct::lower::encode_string(&Md5::digest(format!("{door_id}{i}").as_str()));
+            if hash.starts_with("00000") {
+                if let Some(pos) = hash.chars().nth(5).unwrap().to_digit(10) {
+                    if pos < 8 {
+                        return Some((pos as usize, hash.chars().nth(6).unwrap()));
                     }
                 }
             }
-        }
-    }
+            None
+        })
+        .filter_map(|x| x)
+        .fold(|| ['_'; 8], |mut acc, (pos, ch)| {
+            if acc[pos] == '_' {
+                acc[pos] = ch;
+            }
+            acc
+        })
+        .reduce(|| ['_'; 8], |mut acc, item| {
+            for (i, &ch) in item.iter().enumerate() {
+                if acc[i] == '_' {
+                    acc[i] = ch;
+                }
+            }
+            acc
+        });
+
     output.iter().collect()
 }
