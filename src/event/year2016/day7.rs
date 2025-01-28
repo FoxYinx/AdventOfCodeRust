9fadb6b3692df9b2f74e8e813bd56e6e25df371d
yinx <tollemerkevin@gmail.com>
2025-01-28 08:16:33 +0100

    Year 2016 - Day 7 - Puzzle 2

--- src/event/year2016/day7.rs
+++ src/event/year2016/day7.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs;
 
 pub fn part1() -> u32 {
@@ -24,5 +25,45 @@ pub fn part1() -> u32 {
             valids += 1;
         }
     }
+    valids
+}
+
+pub fn part2() -> u32 {
+    let mut valids = u32::MIN;
+    let file = fs::read_to_string("resources/year2016/day7.txt").expect("Unable to read file!");
+    for line in file.lines() {
+        let mut valid = false;
+        let mut in_brackets = false;
+        let mut outer_bracket_set: HashSet<&[u8]> = HashSet::new();
+        let mut inner_bracket_set: HashSet<&[u8]> = HashSet::new();
+        for window in line.as_bytes().windows(3) {
+            if in_brackets {
+                in_brackets = !window.contains(&93);
+            } else {
+                in_brackets = window.contains(&91);
+            }
+            if window[0] == window[2] && window[0] != window[1] {
+                if in_brackets {
+                    inner_bracket_set.insert(window);
+                } else {
+                    outer_bracket_set.insert(window);
+                }
+            }
+        }
+        for aba in inner_bracket_set {
+            if valid {
+                break;
+            }
+            for &bab in &outer_bracket_set {
+                if aba[0] == bab[1] && aba[1] == bab[0] {
+                    valid = true;
+                }
+            }
+        }
+        if valid {
+            valids += 1;
+        }
+    }
+    
     valids
 }
