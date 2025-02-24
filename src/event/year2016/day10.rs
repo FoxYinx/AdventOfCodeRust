d6af9320301ae2f89f83abd0122829673f953ffd
yinx <tollemerkevin@gmail.com>
2025-02-24 21:04:20 +0100

    Year 2016 - Day 10

--- src/event/year2016/day10.rs
+++ src/event/year2016/day10.rs
@@ -5,63 +5,13 @@ pub fn part1() -> usize {
     bots.sort_by(|a, b| a.number.cmp(&b.number));
     outputs.sort_by(|a, b| a.number.cmp(&b.number));
     add_inputs_bots(&mut bots);
-    let mut flag = false;
-    let mut output = 0;
 
-    loop {
-        for i in 0..bots.len() {
-            if bots[i].values.len() == 2 {
-                bots[i].values.sort_unstable();
-                let mut valid = true;
-                if bots[i].is_lower_output_a_bot() && bots[bots[i].lower_output_number as usize].values.len() >= 2 {
-                    valid = false;
-                }
-                if bots[i].is_higher_output_a_bot() && bots[bots[i].higher_output_number as usize].values.len() >= 2 {
-                    valid = false;
-                }
-                if valid {
-                    let value = bots[i].values[0];
-                    if bots[i].is_lower_output_a_bot() {
-                        let lower_output_number = bots[i].lower_output_number as usize;
-                        let (left, right) = bots.split_at_mut(lower_output_number);
-                        if lower_output_number < left.len() {
-                            left[lower_output_number].values.push(value);
-                        } else {
-                            right[0].values.push(value);
-                        }
-                    } else {
-                        outputs[bots[i].lower_output_number as usize].values.push(value);
-                    }
-                    let value = bots[i].values[1];
-                    if bots[i].is_higher_output_a_bot() {
-                        let higher_output_number = bots[i].higher_output_number as usize;
-                        let (left, right) = bots.split_at_mut(higher_output_number);
-                        if higher_output_number < left.len() {
-                            left[higher_output_number].values.push(value);
-                        } else {
-                            right[0].values.push(value);
-                        }
-                    } else {
-                        outputs[bots[i].higher_output_number as usize].values.push(value);
-                    }
-                    bots[i].values.clear();
-                }
-            }
-            for (j, bot) in bots.iter().enumerate() {
-                if bot.values.contains(&17) && bot.values.contains(&61) {
-                    flag = true;
-                    output = j;
-                    break;
-                }
-            }
-        }
-        
-        if flag {
-            break;
-        }
+    let (flag, output) = process_bots(&mut bots, &mut outputs, true);
+    if flag {
+        output
+    } else {
+        0
     }
-
-    output
 }
 
 pub fn part2() -> u32 {
@@ -69,6 +19,16 @@ pub fn part2() -> u32 {
     bots.sort_by(|a, b| a.number.cmp(&b.number));
     outputs.sort_by(|a, b| a.number.cmp(&b.number));
     add_inputs_bots(&mut bots);
+
+    let (flag, output) = process_bots(&mut bots, &mut outputs, false);
+    if flag {
+        u32::try_from(output).expect("should be a u32")
+    } else {
+        0
+    }
+}
+
+fn process_bots(bots: &mut [Bot], outputs: &mut [Output], check_values: bool) -> (bool, usize) {
     let mut flag = false;
     let mut output = 0;
 
@@ -111,9 +71,20 @@ pub fn part2() -> u32 {
                     bots[i].values.clear();
                 }
             }
-            if !outputs[0].values.is_empty() && !outputs[1].values.is_empty() &!outputs[2].values.is_empty() {
+            if check_values {
+                for (j, bot) in bots.iter().enumerate() {
+                    if bot.values.contains(&17) && bot.values.contains(&61) {
+                        flag = true;
+                        output = j;
+                        break;
+                    }
+                }
+                if flag {
+                    break;
+                }
+            } else if !outputs[0].values.is_empty() && !outputs[1].values.is_empty() && !outputs[2].values.is_empty() {
                 flag = true;
-                output = outputs[0].values[0] * outputs[1].values[0] * outputs[2].values[0];
+                output = (outputs[0].values[0] * outputs[1].values[0] * outputs[2].values[0]) as usize;
                 break;
             }
         }
@@ -123,7 +94,7 @@ pub fn part2() -> u32 {
         }
     }
 
-    output
+    (flag, output)
 }
 
 fn get_initial_bots_and_outputs() -> (Vec<Bot>, Vec<Output>) {
